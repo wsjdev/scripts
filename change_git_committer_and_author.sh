c668e6ac211894138ca2dcf12edb50ea55c2c35a
wsjdev <wsjdev@gmail.com>
2017-02-25 16:42:40 +0900

    [update] インデント幅を 2 に変更

--- change_git_committer_and_author.sh
+++ change_git_committer_and_author.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 if [ $# -ne 3 ]; then
-    echo '引数は、変更元コミッターネーム、変更先名前、変更先メールアドレスの順です。' 1>&2
-    exit 1
+  echo '引数は、変更元コミッターネーム、変更先名前、変更先メールアドレスの順です。' 1>&2
+  exit 1
 fi
 
 git filter-branch -f --commit-filter "
 if [ \"\$GIT_COMMITTER_NAME\" = '$1' ]; then
-    GIT_COMMITTER_NAME='$2'
-    GIT_COMMITTER_EMAIL='$3'
-    GIT_AUTHOR_NAME='$2'
-    GIT_AUTHOR_EMAIL='$3'
+  GIT_COMMITTER_NAME='$2'
+  GIT_COMMITTER_EMAIL='$3'
+  GIT_AUTHOR_NAME='$2'
+  GIT_AUTHOR_EMAIL='$3'
 fi
 git commit-tree \"\$@\"
 " HEAD
